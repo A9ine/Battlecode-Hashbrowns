b21ae9b6c95dc1c7438b85614f0f9976dec585eb
Andrew <andrewzhang2323@gmail.com>
2020-01-13 19:59:41 +0800

    Update HQ.java

--- HQ.java
+++ HQ.java
@@ -11,7 +11,16 @@ public strictfp class HQ extends Building {
     @Override
     public void run() throws GameActionException {
         for (Direction dir : directions){
-            tryBuild(RobotType.MINER, dir);
+            if(tryBuild(RobotType.MINER, dir) && firstRun == false) {
+                rc.build(RobotType.MINER,dir);
+                rc.build(RobotType.MINER,dir);
+                firstRun = true;
+                break;
+            }
+            if(tryBuild(RobotType.MINER, dir) && rc.getTeamSoup()>=70) {
+                rc.build(RobotType.MINER,dir);
+                break;
+            }
         }
     }
 }
