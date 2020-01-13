f26158ab1fca008f84179de9465e7e0c96d7ca30
Andrew <andrewzhang2323@gmail.com>
2020-01-13 17:42:25 +0800

    Update HQ.java

--- HQ.java
+++ HQ.java
@@ -2,6 +2,7 @@ package potato;
 import battlecode.common.*;
 
 public strictfp class HQ extends Building {
+    boolean firstRun = false;
 
     HQ(RobotController rc) throws GameActionException {
         super(rc);
@@ -9,7 +10,10 @@ public strictfp class HQ extends Building {
 
     @Override
     public void run() throws GameActionException {
-
+        for (Direction dir : directions){
+            if (tryBuild(RobotType.MINER, dir) && rc.getSoup()>70) {
+                rc.build(RobotType.MINER,dir);
+            }
+        }
     }
-
 }
