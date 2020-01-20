569128430a5f8b98184a4febfdd85bd181f45599
andronius-yang <andrew.yang_203@outlook.com>
2020-01-20 14:57:49 +0800

    landscaper fuzzymove

--- Landscaper.java
+++ Landscaper.java
@@ -7,6 +7,7 @@ public strictfp class Landscaper extends Unit {
     ArrayList<MapLocation> hqWall = new ArrayList<MapLocation>();
     MapLocation staticWallTarget;
     int wallIter;
+    int storage = 0;
 
     Landscaper(RobotController rc) throws GameActionException {
         super(rc);
@@ -49,16 +50,66 @@ public strictfp class Landscaper extends Unit {
         }
     }
     
-    void landcaperUpdate() throws GameActionException {
+    void landscaperUpdate() throws GameActionException {
         update();
+    }
+    
+    @Override
+    boolean canMoveWithoutSuicide(Direction dir) throws GameActionException{
+        if(!rc.senseFlooding(rc.adjacentLocation(dir)) && !((rc.senseElevation(dir)-3-rc.senseElevation(Direction.CENTER))>25)){
+            if(abs(rc.senseElevation(dir)-rc.senseElevation(Direction.CENTER)) > 3){
+                //up
+                if(rc.senseElevation(dir)-3 > rc.senseElevation(Direction.CENTER)){
+                    for(Direction mdir : Direction){
+                        if(mdir == dir) continue;
+                        while(rc.canDigDirt(mdir)){rc.digDirt(mdir); storage++;}
+                        if(storage >= rc.senseElevation(dir)-3-rc.senseElevation(Direction.CENTER)){
+                            break;
+                        }
+                    }
+                    while(rc.senseElevation(Direction.CENTER) < rc.senseElevation(dir)-3){rc.depositDirt(Direction.CENTER);}
+                    return true;
+                }
+                //down
+                else{
+                    while(rc.senseElevation(Direction.CENTER)-3 > rc.senseElevation(dir)){
+                        if(!rc.canDigDirt(Direction.CENTER)){ //cant dig cuz reach limit 25. dispose some.
+                            for(Direction mdir : Direction){
+                                if(rc.canDepositDirt(mdir) && mdir!=dir){
+                                    rc.depositDirt(mdir);
+                                    break;
+                                }
+                            }
+                        }
+                        rc.digDirt(Direction.CENTER);
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean fuzzyNavigation(MapLocation target) throws GameActionException{
+        rc.setIndicatorLine(myMapLocation, target, 0, 144, 144);
+        if(target.equals(myMapLocation)){
+            return true;
+        }
+        if(rc.isReady()){
+            if(canMoveWithoutSuicide(myMapLocation.directionTo(target))){
+                rc.move(myMapLocation.directionTo(target));
+            }else{
+                bugNavigate(target);
+            }
+        }
     }
 
     @Override
     public void run() throws GameActionException {
-        landcaperUpdate();
+        landscaperUpdate();
 
         if (state == 0) {
-            //I must be a defensive robot
+            //I must be a defensive robot if I'm near the HQ.
             if (hqLoc.distanceSquaredTo(myMapLocation) < 16) {
                 state = 51;
             }
@@ -68,10 +119,12 @@ public strictfp class Landscaper extends Unit {
                 if (rc.senseFlooding(rc.adjacentLocation(myMapLocation.directionTo(hqLoc)))) {
                     if (rc.canDepositDirt(myMapLocation.directionTo(hqLoc))) {
                         rc.depositDirt(myMapLocation.directionTo(hqLoc));
+                        storage--;
                     }
                 }
                 if (rc.canDepositDirt(Direction.CENTER)) {
                     rc.depositDirt(Direction.CENTER);
+                    storage--;
                 }
                 for (Direction dir : directions) {
                     if (rc.adjacentLocation(dir).distanceSquaredTo(hqLoc)<myMapLocation.distanceSquaredTo(hqLoc)) {
@@ -79,6 +132,7 @@ public strictfp class Landscaper extends Unit {
                     }
                     if (!hqWall.contains(rc.adjacentLocation(dir)) && rc.canDigDirt(dir)) {
                         rc.digDirt(dir);
+                        storage++;
                     }
                 }
             } else {
@@ -98,10 +152,12 @@ public strictfp class Landscaper extends Unit {
                             if (getAdjacent().contains(staticWallTarget)){
                                 if (rc.canDepositDirt(myMapLocation.directionTo(staticWallTarget))) {
                                     rc.depositDirt(myMapLocation.directionTo(staticWallTarget));
+                                    storage--;
                                 }
                                 for (Direction dir : directions) {
                                     if (!hqWall.contains(rc.adjacentLocation(dir)) && rc.canDigDirt(dir)) {
                                         rc.digDirt(dir);
+                                        storage++;
                                     }
                                 }
                             }
